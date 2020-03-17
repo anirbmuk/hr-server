9528420024539bfaeffb1b85556ec465c61fda65
anirbmuk <anirbmuk@live.in>
2020-03-17 14:26:04 +0530

    Added heroku domain to access control

--- src/server.js
+++ src/server.js
@@ -8,11 +8,16 @@ const locationRoutes = require('./routes/location.route');
 const PORT = process.env.PORT || '3000';
 const base_path = process.env.hr_server_base_path;
 
+const allowedOrigins = ['http://localhost:8000', 'https://menj-server.herokuapp.com'];
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
-  next();
+	const origin = req.headers.origin;
+	if (allowedOrigins.indexOf(origin) > -1) {
+		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+	}
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
+	next();
 });
 
 app.use(express.json());
