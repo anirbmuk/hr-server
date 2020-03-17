750552e6f64ba753f2054693a1a06f42427b15b3
anirbmuk <anirbmuk@live.in>
2020-03-17 08:57:54 +0530

    Added headers for requests

--- src/server.js
+++ src/server.js
@@ -8,6 +8,13 @@ const locationRoutes = require('./routes/location.route');
 const PORT = process.env.PORT || '3000';
 const base_path = process.env.hr_server_base_path;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
+  next();
+});
+
 app.use(express.json());
 
 app.use(base_path + 'locations', locationRoutes);
