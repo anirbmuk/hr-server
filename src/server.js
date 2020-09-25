789eb992165cd6e3bec47f2fe16d387f445de650
anirbmuk <anirbmuk@live.in>
2020-09-25 12:05:11 +0530

    Added accept-language header

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 	if (allowedOrigins.indexOf(origin) > -1) {
 		res.setHeader('Access-Control-Allow-Origin', origin);
 	}
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, accept-language');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
 	next();
 });
