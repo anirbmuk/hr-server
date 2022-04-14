10f1fafd921875fc81ea72e0a0f693f7604fdfca
anirbmuk <anirbmuk@live.in>
2022-04-14 20:50:18 +0200

    chore: updated npm dependencies

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -11,8 +11,6 @@ if (app_environment === 'dev') {
 }
 
 mongoose.connect(connectionURL, {
-    useCreateIndex: true,
-    useFindAndModify: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).catch(() => console.error('Failed to connect to mongodb'));
